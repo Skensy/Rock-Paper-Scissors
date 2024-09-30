4f9824b4a3bb5a21298b26c064ba63c760b47164
Skensy <11029252+Skensy@users.noreply.github.com>
2024-09-30 10:50:13 +0200

    make it work with chatgpt tipos

--- script.js
+++ script.js
@@ -1,63 +1,56 @@
-let humanScore = 0
-let computerScore = 0
-
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(max) {
-    let number = Math.floor(Math.random() * max);
-    if (number === 0) {
-        let computerRock = "rock"
-    } else if (number === 1) {
-        let computerPaper = "paper"
-    } else if (number === 2) {
-        let computerScissors = "scissors"
-    }
+  let number = Math.floor(Math.random() * max);
+  if (number === 0) {
+    return "rock";
+  } else if (number === 1) {
+    return "paper";
+  } else if (number === 2) {
+    return "scissors";
+  }
 }
 
 function getHumanChoice() {
-    let request = prompt("Stiamo giocando a carta-forbice-sasso. Qual è la tua scelta?");
-    if (request === "rock") {
-        let humanRock = console.log("rock")
-    } else if (request === "paper") {
-        let humanPaper = console.log("paper")
-    } else if (request === "scissors") {
-        let humanScissors = console.log("scissors") 
-    } else {
-        getHumanChoice()
-    }
+  let request = prompt("Stiamo giocando a carta-forbice-sasso. Qual è la tua scelta?");
+  if (request === "rock" || request === "paper" || request === "scissors") {
+    console.log(request);
+    return request;
+  } else {
+    return getHumanChoice();
+  }
 }
-
 
 function playRound(computerChoice, humanChoice) {
-    computerChoice = getComputerChoice(3);
-    humanChoice = getHumanChoice();
-    if (computerChoice === "rock" && humanChoice === "paper") {
-        console.log("Hai vinto!")
-    } else if (computerChoice === "rock" && humanChoice === "scissors") {
-        console.log("Il computer ha vinto")
-    } else if (computerChoice === "rock" && humanChoice === "rock") {
-        console.log("Pareggio!")
-    }
+  computerChoice = getComputerChoice(3);
+  humanChoice = getHumanChoice();
 
-    if (computerChoice === "paper" && humanChoice === "paper") {
-        console.log("Pareggio!")
-    } else if (computerChoice === "paper" && humanChoice === "scissors") {
-        console.log("Hai vinto")
-    } else if (computerChoice === "paper" && humanChoice === "rock") {
-        console.log("Il computer ha vinto!")
-    } 
-
-    if (computerChoice === "scissors" && humanChoice === "paper") {
-        console.log("Il computer ha vinto!")
-    } else if (computerChoice === "scissors" && humanChoice === "scissors") {
-        console.log("Pareggio!")
-    } else if (computerChoice === "scissors" && humanChoice === "rock") {
-        console.log("Hai vinto!")
-    }
+  if (computerChoice === "rock" && humanChoice === "paper") {
+    console.log("Hai vinto!");
+    humanScore++;
+  } else if (computerChoice === "rock" && humanChoice === "scissors") {
+    console.log("Il computer ha vinto");
+    computerScore++;
+  } else if (computerChoice === "rock" && humanChoice === "rock") {
+    console.log("Pareggio!");
+  } else if (computerChoice === "paper" && humanChoice === "paper") {
+    console.log("Pareggio!");
+  } else if (computerChoice === "paper" && humanChoice === "scissors") {
+    console.log("Hai vinto!");
+    humanScore++;
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    console.log("Il computer ha vinto!");
+    computerScore++;
+  } else if (computerChoice === "scissors" && humanChoice === "paper") {
+    console.log("Il computer ha vinto!");
+    computerScore++;
+  } else if (computerChoice === "scissors" && humanChoice === "scissors") {
+    console.log("Pareggio!");
+  } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    console.log("Hai vinto!");
+    humanScore++;
+  }
 }
 
-playRound()
-
-/*
-getComputerChoice(3);
-getHumanChoice();
-*/
+playRound();
